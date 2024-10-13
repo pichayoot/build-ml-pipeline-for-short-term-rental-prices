508399d2d03831a797365ea97aa736c0c797685e
Pichayoot Ouppaphan <pichayoot@gmail.com>
2024-10-13 19:31:06 -0400

    add longitude and latitude filter

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -43,6 +43,11 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Filter longitude and latitude
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+
     logger.info("Saving cleaned data as artifact")
     df.to_csv(args.output_artifact, index=False)
 
